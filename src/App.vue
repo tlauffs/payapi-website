4171edbad964eb7a605122e6b1663030368fb271
Your Name <timlauffs@gmail.com>
2022-07-14 19:35:38 +0200

    initial commit / set up basic styles and html

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const mobile_menu = ref(null);
+
+function openNav(){
+    mobile_menu.value.style.right = "0";
+}
+
+function closeNav(){
+    mobile_menu.value.style.right = "-80%";
+}
+
+</script>
+
+<template>
+  <header>
+    <nav class="nav-container">
+        <router-link to="/">
+        </router-link>
+        <div class="desktop-nav">
+            <router-link to="/pricing" class="nav-element"><b>Pricing</b></router-link>
+            <router-link to="/about" class="nav-element"><b>About</b></router-link>
+            <router-link to="/contact" class="nav-element"><b>Contact</b></router-link>
+            <button class="btn-primary nav-button"><b>Schedule a Demo</b></button>
+        </div>
+        <div class="mobile-nav">
+            <img src="/src/assets/shared/mobile/menu.svg" @click="openNav()" class="hamburger-btn" alt="">
+            <div ref="mobile_menu" class="mobile-menu">
+                <div class="mobile-menu-nav">
+                    <img src="/src/assets/shared/mobile/close.svg" @click="closeNav()" class="close-btn" alt="">
+                    <div class="divider"></div>
+                    <router-link to="/pricing" class="nav-element nav-element-mobile"><b>Pricing</b></router-link>
+                    <router-link to="/about" class="nav-element nav-element-mobile"><b>About</b></router-link>
+                    <router-link to="/contact" class="nav-element nav-element-mobile"><b>Contact</b></router-link>
+                    <button class="btn-primary btn-nav-mobile"><b>Schedule a Demo</b></button>
+                </div>
+            </div>
+        </div>
+    </nav>
+  </header>
+  <router-view></router-view>
+</template>
+
+<style scoped>
+</style>
---
+++ src/router/index.ts
@@ -0,0 +1,33 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../components/Home.vue";
+import About from "../components/About.vue";
+import Contact from "../components/Contact.vue";
+import Pricing from "../components/Pricing.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: Home,
+    },
+    {
+      path: "/about",
+      name: "about",
+      component: About,
+    },
+    {
+      path: "/contact",
+      name: "contact",
+      component: Contact,
+    },
+    {
+      path: "/pricing",
+      name: "pricing",
+      component: Pricing,
+    },
+  ],
+});
+
+export default router;
