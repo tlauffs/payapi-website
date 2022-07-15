1554afecee685006ace62b2d551cb8f7f5cb0503
Your Name <timlauffs@gmail.com>
2022-07-16 01:29:04 +0200

    worked on home page

--- src/App.vue
+++ src/App.vue
@@ -3,45 +3,165 @@ import { ref } from "vue";
 
 const mobile_menu = ref(null);
 
-function openNav(){
-    mobile_menu.value.style.right = "0";
+function openNav() {
+  mobile_menu.value.style.right = "0";
 }
 
-function closeNav(){
-    mobile_menu.value.style.right = "-80%";
+function closeNav() {
+  mobile_menu.value.style.right = "-80%";
 }
-
 </script>
 
 <template>
   <header>
     <nav class="nav-container">
-        <router-link to="/">
-            <img src="/src/assets/shared/desktop/logo.svg" class="nav-logo" alt="">
-        </router-link>
-        <div class="desktop-nav">
-            <router-link to="/pricing" class="nav-element"><b>Pricing</b></router-link>
-            <router-link to="/about" class="nav-element"><b>About</b></router-link>
-            <router-link to="/contact" class="nav-element"><b>Contact</b></router-link>
-            <button class="btn-primary nav-button"><b>Schedule a Demo</b></button>
+      <router-link to="/">
+        <img
+          src="/src/assets/shared/desktop/logo.svg"
+          class="nav-logo"
+          alt=""
+        />
+      </router-link>
+      <div class="desktop-nav">
+        <router-link to="/pricing" class="nav-element"
+          ><b>Pricing</b></router-link
+        >
+        <router-link to="/about" class="nav-element"><b>About</b></router-link>
+        <router-link to="/contact" class="nav-element"
+          ><b>Contact</b></router-link
+        >
+        <button class="btn-primary nav-button"><b>Schedule a Demo</b></button>
+      </div>
+      <div class="mobile-nav">
+        <img
+          src="/src/assets/shared/mobile/menu.svg"
+          @click="openNav()"
+          class="hamburger-btn"
+          alt=""
+        />
+        <div ref="mobile_menu" class="mobile-menu">
+          <div class="mobile-menu-nav">
+            <img
+              src="/src/assets/shared/mobile/close.svg"
+              @click="closeNav()"
+              class="close-btn"
+              alt=""
+            />
+            <div class="divider"></div>
+            <router-link to="/pricing" class="nav-element"
+              ><b>Pricing</b></router-link
+            >
+            <router-link to="/about" class="nav-element"
+              ><b>About</b></router-link
+            >
+            <router-link to="/contact" class="nav-element"
+              ><b>Contact</b></router-link
+            >
+            <button class="btn-primary btn-nav-mobile">
+              <b>Schedule a Demo</b>
+            </button>
+          </div>
         </div>
-        <div class="mobile-nav">
-            <img src="/src/assets/shared/mobile/menu.svg" @click="openNav()" class="hamburger-btn" alt="">
-            <div ref="mobile_menu" class="mobile-menu">
-                <div class="mobile-menu-nav">
-                    <img src="/src/assets/shared/mobile/close.svg" @click="closeNav()" class="close-btn" alt="">
-                    <div class="divider"></div>
-                    <router-link to="/pricing" class="nav-element nav-element-mobile"><b>Pricing</b></router-link>
-                    <router-link to="/about" class="nav-element nav-element-mobile"><b>About</b></router-link>
-                    <router-link to="/contact" class="nav-element nav-element-mobile"><b>Contact</b></router-link>
-                    <button class="btn-primary btn-nav-mobile"><b>Schedule a Demo</b></button>
-                </div>
-            </div>
-        </div>
+      </div>
     </nav>
   </header>
   <router-view></router-view>
+  <div class="footer">
+    <div class="section flex-container pt-5">
+      <div class="mb-3">
+        <h2>Ready to start?</h2>
+      </div>
+      <div class="input-wrapper mb-3">
+        <input class="input" placeholder="Enter email address " type="text" />
+        <button class="btn-primary btn-demo">Schedule a Demo</button>
+      </div>
+    </div>
+    <div class="flex-footer mt-3">
+      <div class="nav-footer-ele">
+        <router-link to="/">
+        <img
+          src="/src/assets/shared/desktop/logo.svg"
+          class="nav-logo logo-white"
+          alt=""
+        />
+        </router-link>
+      </div>
+      <router-link
+        to="/pricing"
+        class="nav-element-light white-text nav-footer-ele"
+        ><b>Pricing</b></router-link
+      >
+      <router-link
+        to="/about"
+        class="nav-element-light white-text nav-footer-ele"
+        ><b>About</b></router-link
+      >
+      <router-link
+        to="/contact"
+        class="nav-element-light white-text nav-footer-ele"
+        ><b>Contact</b></router-link
+      >
+      <div class="socials nav-footer-ele">
+        <img src="/src/assets/shared/desktop/facebook.svg" alt="" class="mlr social-btn" />
+        <img src="/src/assets/shared/desktop/twitter.svg" alt="" class="mlr social-btn" />
+        <img src="/src/assets/shared/desktop/linkedin.svg" alt="" class="mlr social-btn" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
+/* footer styles*/
+.flex-container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
+}
+.flex-footer {
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  padding: 2rem clamp(0rem, 10.375rem, 7vw) 1.2rem clamp(0rem, 10.375rem, 7vw);
+  background-color: var(--dark-blue);
+}
+.logo-white {
+  filter: invert(100%) sepia(100%) saturate(0%) hue-rotate(288deg)
+    brightness(102%) contrast(102%);
+}
+.nav-element:hover,
+.nav-element:focus .nav-element:active {
+  color: var(--light-blue);
+}
+.socials {
+  margin-left: auto;
+}
+
+.social-btn{
+    transition: all 250ms;
+    cursor: pointer;
+}
+
+.social-btn:hover,
+.social-btn:focus,
+.social-btn:active  {
+  opacity: 1;
+  filter: invert(71%) sepia(15%) saturate(7094%) hue-rotate(297deg) brightness(72%) contrast(54%);
+}
+@media (max-width: 64rem) {
+  .flex-container {
+    justify-content: center;
+  }
+}
+@media (max-width: 48rem) {
+  .flex-footer {
+    flex-direction: column;
+  }
+  .socials {
+    margin-left: 0;
+  }
+  .nav-footer-ele {
+    margin-bottom: 2rem;
+  }
+}
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,154 +1,243 @@
 <script setup lang="ts">
-
 </script>
 <template>
-    <div>
+  <div>
+    <!--
     <div class="circle top"></div>
+    -->
+  </div>
+  <div class="grid-container section">
+    <div class="grid-item header-text">
+      <h1>Start building with our APIs for absolutely free.</h1>
+      <div class="input-wrapper demo-input">
+        <input class="input" placeholder="Enter email address " type="text" />
+        <button class="btn-primary btn-demo">Schedule a Demo</button>
+      </div>
+      <p class="indent">
+        Have any questions?
+        <router-link to="/contact" class="btn-link"
+          ><b>Contact Us</b></router-link
+        >
+      </p>
     </div>
-    <div class="grid-container section">
-        <div class="grid-item header-text">
-            <h1>Start building with our APIs for absolutely free.</h1>
-            <div class="input-wrapper demo-input">
-                <input class="input" placeholder="Enter email address " type="text">
-                <button class="btn-primary btn-demo">Schedule a Demo</button>
-            </div>
-            <p  class="indent">Have any questions?  <router-link to="/contact" class="btn-link"><b>Contact Us</b></router-link></p>
-        </div>
-        <div class="grid-item phone-item">
-            <img src="/src/assets/home/desktop/illustration-phone-mockup.svg" class="phone-img" alt="">
-        </div>
+    <div class="grid-item phone-item">
+      <img
+        src="/src/assets/home/desktop/illustration-phone-mockup.svg"
+        class="phone-img"
+        alt=""
+      />
     </div>
-    <div class="dark-section">
-        <div class="grid-container">
-        <div class="section-who">
-            <h2 class="white-text">Who we work with</h2>
-            <p class="white-text opaque">Today, millions of people around the world have successfully connected 
-    their accounts to apps they love using our API. We provide developers 
-    with the tools they need to create easy and accessible experiences 
-    for their users.</p>
-            <div class="mt-3 mb">
-                <router-link to="/about" class="btn-sec-light">About Us</router-link>
-            </div>
+  </div>
+  <div class="dark-section">
+    <div class="grid-container">
+      <div class="max-text-width">
+        <h2 class="white-text">Who we work with</h2>
+        <p class="white-text opaque">
+          Today, millions of people around the world have successfully connected
+          their accounts to apps they love using our API. We provide developers
+          with the tools they need to create easy and accessible experiences for
+          their users.
+        </p>
+        <div class="mt-3 mb">
+          <router-link to="/about" class="btn-sec-light">About Us</router-link>
         </div>
-        <div class="logo-grid-wrapper">
-            <div class="logo-grid">
-                <div><img src="/src/assets/shared/desktop/tesla.svg" alt=""></div>
-                <div><img src="/src/assets/shared/desktop/microsoft.svg" alt=""></div>
-                <div><img src="/src/assets/shared/desktop/hewlett-packard.svg" alt=""></div>
-                <div><img src="/src/assets/shared/desktop/oracle.svg" alt=""></div>
-                <div><img src="/src/assets/shared/desktop/google.svg" alt=""></div>
-                <div><img src="/src/assets/shared/desktop/nvidia.svg" alt=""></div>
-            </div>
+      </div>
+      <div class="logo-grid-wrapper">
+        <div class="logo-grid">
+          <div><img src="/src/assets/shared/desktop/tesla.svg" alt="" /></div>
+          <div>
+            <img src="/src/assets/shared/desktop/microsoft.svg" alt="" />
+          </div>
+          <div>
+            <img src="/src/assets/shared/desktop/hewlett-packard.svg" alt="" />
+          </div>
+          <div><img src="/src/assets/shared/desktop/oracle.svg" alt="" /></div>
+          <div><img src="/src/assets/shared/desktop/google.svg" alt="" /></div>
+          <div><img src="/src/assets/shared/desktop/nvidia.svg" alt="" /></div>
         </div>
+      </div>
     </div>
+  </div>
+  <div class="grid-container section pt-5">
+    <div>
+      <img
+        src="/src/assets/home/desktop/illustration-easy-to-implement-cropped.svg"
+        class="implement-img"
+        alt=""
+      />
     </div>
-
-    Easy to implement
-    Our API comes with just a few lines of code. You’ll be up and running in 
-    no time. We built our documentation page to integrate payments functionality 
-    with ease.
-
-    Simple UI & UX
-    Our pre-built form is easy to integrate in your app or website’s checkout 
-    flow and designed to optimize conversion.
-
-    Personal Finances
-    Consolidate financial data from multiple sources and categorize transactions up to
-    2 years of history. Analyze reports to reconcile activities in your account.
-
-    Banking & Coverage
-    With our platform, you can speed up account onboarding and support ongoing payments 
-    for checking, savings, credit card, and brokerage accounts.
-
-    Consumer Payments
-    It’s easier to set up secure bank payments with us through a flow designed with the 
-    user experience in mind. Customers could instantly authenticate their account.
-
-    Ready to start? 
-    Enter email address 
-    Schedule a Demo
-
-    Pricing
-    About
-    Contact
+    <div class="grid-item">
+      <div class="implement-text">
+        <h2>Easy to implement</h2>
+        <p>
+          Our API comes with just a few lines of code. You’ll be up and running
+          in no time. We built our documentation page to integrate payments
+          functionality with ease.
+        </p>
+      </div>
+    </div>
+  </div>
+  <div class="grid-container section pt-5">
+    <div class="grid-item">
+      <div class="implement-text mr-2">
+        <h2>Simple UI & UX</h2>
+        <p>
+          Our pre-built form is easy to integrate in your app or website’s
+          checkout flow and designed to optimize conversion.
+        </p>
+      </div>
+    </div>
+    <div class="phone-item">
+      <img
+        src="/src/assets/home/desktop/illustration-simple-ui.svg"
+        class="ui-img"
+        alt=""
+      />
+    </div>
+  </div>
+  <div class="grid-container-3-col section pt-5">
+    <div>
+      <img src="/src/assets/home/desktop/icon-personal-finances.svg" />
+      <p class="p-header">Personal Finances</p>
+      <p>
+        Consolidate financial data from multiple sources and categorize
+        transactions up to 2 years of history. Analyze reports to reconcile
+        activities in your account.
+      </p>
+    </div>
+    <div>
+      <img src="/src/assets/home/desktop/icon-banking-and-coverage.svg" />
+      <p class="p-header">Banking & Coverage</p>
+      <p>
+        With our platform, you can speed up account onboarding and support
+        ongoing payments for checking, savings, credit card, and brokerage
+        accounts.
+      </p>
+    </div>
+    <div>
+      <img src="/src/assets/home/desktop/icon-consumer-payments.svg" />
+      <p class="p-header">Consumer Payments</p>
+      <p>
+        It’s easier to set up secure bank payments with us through a flow
+        designed with the user experience in mind. Customers could instantly
+        authenticate their account.
+      </p>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-.top{
-    position: absolute;
-    overflow: hidden;
-    top:-12rem;
-    right:-12rem;
+.top {
+  position: absolute;
+  overflow: hidden;
+  top: -12rem;
+  right: -12rem;
 }
 
+/* layout styles */
+
 .grid-container {
-    display: grid;
-    align-items: center;
-    grid-template-columns: repeat(2, 1fr);
-    grid-template-rows: 1fr;
-    grid-column-gap: 0px;
-    grid-row-gap: 0px;
+  display: grid;
+  align-items: center;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: 1fr;
+  grid-column-gap: 0px;
+  grid-row-gap: 0px;
 }
-.grid-item{
+.grid-item {
   width: 100%;
   display: flex;
   flex-direction: column;
   justify-content: center;
 }
-.phone-img{
-    margin-left: clamp(0px,15%,120px);
-    max-width: clamp(0rem,50rem,100%);
+.phone-img {
+  margin-left: clamp(0px, 15%, 120px);
+  max-width: clamp(0rem, 50rem, 100%);
 }
 .header-text {
-    margin-bottom: 5rem;
+  margin-bottom: 5rem;
 }
-.demo-input{
-    margin-top: 2rem;
+.demo-input {
+  margin-top: 2rem;
 }
 .logo-grid {
-    --n: 3;
-    display: grid;
-    grid-template-columns: repeat(auto-fill,
-           minmax(max(10rem,(100% - (var(--n) - 1)*1rem)/var(--n)), 1fr));
-    gap: 1rem;
-    justify-items: center;
-    align-items: center;
+  --n: 3;
+  display: grid;
+  grid-template-columns: repeat(
+    auto-fill,
+    minmax(max(10rem, (100% - (var(--n) - 1) * 1rem) / var(--n)), 1fr)
+  );
+  gap: 1rem;
+  justify-items: center;
+  align-items: center;
 }
 
 .logo-grid-wrapper {
-    width: 100%;
+  width: 100%;
 }
-.section-who {
-    max-width:28rem;
+.max-text-width {
+  max-width: 28rem;
 }
+.implement-text {
+  max-width: 38rem;
+}
+.implement-img {
+  width: min(80vw, 28rem);
+  margin-right: 1rem;
+}
+.ui-img {
+  width: min(80vw, 35rem);
+
+  margin-right: 1rem;
+}
+.grid-container-3-col {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 7%;
+  text-align: center;
+}
+
 /* responsiveness query */
+@media (max-width: 48rem) {
+  .grid-container-3-col {
+    grid-template-columns: 1fr;
+    gap: 1rem;
+  }
+}
 @media (max-width: 64rem) {
-    .grid-container {
-        grid-template-columns: 1fr;
-        grid-template-rows: 2;
-        justify-items: center;
-        text-align: center;
-    }
-    .phone-img{
-        margin: 0 auto;
-        padding-left: 8%;
-        max-width: clamp(0rem,25rem,100%);
-    } 
-    .phone-item{
-        grid-row: 1;
-    } 
-    .demo-input{
-        margin-left: auto;
-        margin-right: auto;
-    }
-    .top{
-        top:calc(-100vw - 20rem);
-        left: 50%;
-        transform: translate(-50%, 0);
-    }
-    .logo-grid-wrapper {
-        grid-row: 1;
-        margin-bottom: 4rem;
-    }
+  .grid-container {
+    grid-template-columns: 1fr;
+    grid-template-rows: 2;
+    justify-items: center;
+    text-align: center;
+  }
+  .phone-img {
+    margin: 0 auto;
+    padding-left: 8%;
+    max-width: clamp(0rem, 25rem, 100%);
+  }
+  .implement-text {
+    margin: 0 auto;
+  }
+  .implement-img {
+    margin-right: 0;
+    margin-bottom: 3rem;
+  }
+  .phone-item {
+    grid-row: 1;
+  }
+  .demo-input {
+    margin-left: auto;
+    margin-right: auto;
+  }
+  .top {
+    top: calc(-100vw - 20rem);
+    left: 50%;
+    transform: translate(-50%, 0);
+  }
+  .logo-grid-wrapper {
+    grid-row: 1;
+    margin-bottom: 4rem;
+  }
 }
 </style>
